de99d9ed43c5f65d743d66ce0ca3c3e0a19acd22
Simen A. W. Olsen <so@bjerk.io>
2022-09-03 14:13:31 +0200

    add working billing api

---
+++ src/billing-api/config.ts
@@ -0,0 +1,7 @@
+import * as pulumi from '@pulumi/pulumi';
+
+const config = new pulumi.Config('billing-api');
+
+export const apiDomain = 'api.billing.cc.ayr.no';
+export const tag = config.require('tag');
+export const image = config.require('image');
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 import './ingress-controller';
 import './lago/api';
 import './lago/ingress';
+import './billing-api/deployment';
+import './billing-api/ingress';
+import './postgres-operator';
+import './billing-api/db';
