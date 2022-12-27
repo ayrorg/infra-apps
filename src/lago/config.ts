06739b882feda5ec619ddcdc1cdc09ce85bba27c
Simen A. W. Olsen <so@bjerk.io>
2022-12-28 00:30:54 +0100

    bump lago to v0.19.2-alpha

--- src/lago/config.ts
+++ src/lago/config.ts
@@ -1,2 +1,8 @@
+import { Config } from '@pulumi/pulumi';
+
+const config = new Config('lago');
+
 export const frontendDomain = 'lago.cc.ayr.no';
 export const apiDomain = 'api.lago.cc.ayr.no';
+
+export const tag = config.require('tag');
